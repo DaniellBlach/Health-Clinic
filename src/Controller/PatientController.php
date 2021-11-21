ae7de5d7fcbd3fc8cdfc88f014bd9b4fc61aef9b
Daniel <dandys9987@gmail.com>
2021-11-21 18:30:06 +0100

    :sparkles: applied basic roles admin and user :zap: now admin can see every patient and edit his data closes #12

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -4,12 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Patient;
 use App\Form\PatientType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
+/**
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
 class PatientController extends AbstractController
 {
     private $security;
@@ -40,16 +44,37 @@ class PatientController extends AbstractController
     {
         $form = $this->createForm(PatientType::class, $patient);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->flush();
             $this->addFlash('success', 'Pomyślnie zmieniono dane');
+            $hasAccess = $this->isGranted('ROLE_ADMIN');
+            if($hasAccess) {
+                return $this->redirectToRoute('patients');
+            }
+            else{
+                return $this->redirectToRoute('patient');
+            }
 
-            return $this->redirectToRoute('patient');
         }
 
         return $this->render('patient/edit.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+    /**
+     *
+     * @Route("/patients", name="patients")
+     * @IsGranted("ROLE_ADMIN")
+     * @param Request $request
+     * @return Response
+     */
+    public function allPatients(Request $request): Response
+    {
+        $repository=$this->getDoctrine()->getRepository(Patient::class);
+        return $this->render('patient/patients.html.twig', [
+            'patients'=>$repository->findall()
         ]);
     }
 }
