a132a5b56b3b7e3c7e17a35c32d942c63bb974a6
Daniel <dandys9987@gmail.com>
2021-12-09 20:03:51 +0100

    :sparkles: Now adminstrator can add new employees to the system

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Employee;
+use App\Entity\User;
+use App\Form\EmployeeType;
+use App\Form\RegistrationFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 
@@ -26,6 +32,40 @@ class EmployeeController extends AbstractController
         $repository = $this->getDoctrine()->getRepository(Employee::class);
         return $this->render('employee/index.html.twig', [
             'employee' => $repository->find($user->getemployeeid())
+        ]);
+    }
+
+    /**
+     * @Route("/employee/add", name="add_employee")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function add(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+        $employee = new Employee();
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $formEmployee = $this->createForm(EmployeeType::class,$employee);
+        $form->handleRequest($request);
+        $formEmployee->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid() && $formEmployee->isSubmitted() && $formEmployee->isValid()) {
+
+            $user->setPassword(
+                $userPasswordHasherInterface->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $employee->setUserid($user);
+            $user->setEmployeeid($employee);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->persist($employee);
+            $entityManager->flush();
+            return $this->redirectToRoute('index');
+        }
+        return $this->render('employee/add.html.twig', [
+            'registrationForm' => $form->createView(),
+            'employeeForm'=>$formEmployee->createView(),
         ]);
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,6 +20,14 @@ class RegistrationFormType extends AbstractType
             ->add('email', null, [
                 'label' => 'E-mail',
                 'attr' => ['class' => 'form-control'],
+            ])
+            ->add('roles',ChoiceType::class,[
+                'label' => 'Rola pracownika',
+                'attr' => ['class' => 'form-control'],
+                'choices' => [
+                    'administrator systemu' => 'ROLE_ADMIN',
+                    'pracownik administracyjny' => 'ROLE_EMPLOYEE',
+                ]
             ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
@@ -34,7 +44,19 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ]);
+            ])
+            ->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
