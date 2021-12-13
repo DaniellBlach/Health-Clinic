86aab4ea619f41a8f46bee102fb75d888a7a9fbe
Daniel <dandys9987@gmail.com>
2021-12-13 21:05:37 +0100

    :sparkles: created doctor fixtures

--- src/DataFixtures/EmployeeFixtures.php
+++ src/DataFixtures/EmployeeFixtures.php
@@ -13,29 +13,36 @@ class EmployeeFixtures extends Fixture implements DependentFixtureInterface
     {
         $this->loadPatients($manager);
     }
+
     private function loadPatients(ObjectManager $manager)
     {
         $i = 3;
+        $j=0;
         foreach ($this->getEmployeeData() as [$name, $surname]) {
             $employee = new Employee();
             $employee
                 ->setName($name)
                 ->setSurname($surname)
                 ->setUserid($this->getReference('user_' . $i));
-            $user=$this->getReference('user_' . $i++);
+            $user = $this->getReference('user_' . $i++);
+            $this->addReference('employee_' . $j++, $employee);
             $manager->persist($employee);
             $user->setEmployeeid($employee);
         }
 
         $manager->flush();
     }
+
     private function getEmployeeData()
     {
         return [
             ['Piotr', 'Nowak'],
             ['Alicja', 'Kowalska'],
+            ['Joanna', 'Ptasińska'],
+            ['Andrzej', 'Zając'],
         ];
     }
+
     public function getDependencies(): array
     {
         return [
