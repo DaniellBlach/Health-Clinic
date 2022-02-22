eb18faa69018057fb43667c9dfeacb44fd0514d7
Daniel <dandys9987@gmail.com>
2022-02-22 12:29:21 +0100

    :sparkles: added flatpickr class for time

--- assets/javascript/flatpickr.js
+++ assets/javascript/flatpickr.js
@@ -2,8 +2,15 @@ import flatpickr from 'flatpickr';
 import {Polish} from "flatpickr/dist/l10n/pl";
 
 const datePicker = document.querySelectorAll(".datePicker");
+const timePicker = document.querySelectorAll(".timePicker")
 
 flatpickr(datePicker, {
     locale: Polish,
     enableTime: false,
+});
+flatpickr(timePicker, {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true
 });
