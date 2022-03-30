1088d84bebc7fd9cf263e4c2e1633615caa1bc65
Daniel <dandys9987@gmail.com>
2022-03-30 18:11:31 +0200

    :zap: added data to new columns in patient fixtures

--- src/DataFixtures/PatientFixtures.php
+++ src/DataFixtures/PatientFixtures.php
@@ -14,10 +14,11 @@ class PatientFixtures extends Fixture implements DependentFixtureInterface
     {
         $this->loadPatients($manager);
     }
+
     private function loadPatients(ObjectManager $manager)
     {
         $i = 0;
-        foreach ($this->getPatientsData() as [$name, $surname, $pesel, $date_of_birth, $sex, $phone_number]) {
+        foreach ($this->getPatientsData() as [$name, $surname, $pesel, $date_of_birth, $sex, $phone_number, $date_of_joining]) {
             $patient = new Patient();
             $patient
                 ->setName($name)
@@ -26,28 +27,32 @@ class PatientFixtures extends Fixture implements DependentFixtureInterface
                 ->setDateOfBirth($date_of_birth)
                 ->setSex($sex)
                 ->setPhoneNumber($phone_number)
-                ->setUser($this->getReference('user_' . $i));
+                ->setUser($this->getReference('user_' . $i))
+                ->setDateOfJoining($date_of_joining)
+                ->setDoctorOfFirstContact($this->getReference('doctor_0'));
             $this->addReference('patient_' . $i, $patient);
-            $user=$this->getReference('user_' . $i++);
+            $user = $this->getReference('user_' . $i++);
             $manager->persist($patient);
             $user->setPatient($patient);
         }
 
         $manager->flush();
     }
+
     private function getPatientsData()
     {
         return [
-            ['Jan', 'Nowak', '12312312311',DateTime::createFromFormat('d.m.Y', '11.11.2000'),'mężczyzna','123123123'],
-            ['Ala', 'Kowalska', '12312312312',DateTime::createFromFormat('d.m.Y', '01.12.1990'),'kobieta','123456789'],
-            ['Michał', 'Kowalski', '12312312313',DateTime::createFromFormat('d.m.Y', '23.11.1973'),'mężczyzna','987654321'],
+            ['Jan', 'Nowak', '12312312311', DateTime::createFromFormat('d.m.Y', '11.11.2000'), 'mężczyzna', '123123123', DateTime::createFromFormat('d.m.Y', '1.1.2022')],
+            ['Ala', 'Kowalska', '12312312312', DateTime::createFromFormat('d.m.Y', '01.12.1990'), 'kobieta', '123456789', DateTime::createFromFormat('d.m.Y', '2.2.2022')],
+            ['Michał', 'Kowalski', '12312312313', DateTime::createFromFormat('d.m.Y', '23.11.1973'), 'mężczyzna', '987654321', DateTime::createFromFormat('d.m.Y', '3.3.2022')],
         ];
     }
 
     public function getDependencies(): array
     {
         return [
-            UserFixtures::class
+            UserFixtures::class,
+            DoctorFixtures::class
         ];
     }
 }
