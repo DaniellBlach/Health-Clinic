990c984bd6bd228ffb988c745b57ab06cd989aca
Daniel <dandys9987@gmail.com>
2022-04-04 17:58:53 +0200

    :art: changed name of route

--- src/Controller/MedicalVisitController.php
+++ src/Controller/MedicalVisitController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MedicalVisitController extends AbstractController
 {
     /**
-     * @Route("/medical/visits/{patient}", name="medical_visit")
+     * @Route("/medical/visits/{patient}", name="medical_visits")
      */
     public function index(Patient $patient): Response
     {
