e813c4ff3a793a682afb22ac59e58f5050efc6cd
Daniel <dandys9987@gmail.com>
2022-04-04 12:49:55 +0200

    :sparkles: created medical visit controller, added function which allow to see patients medical visits,added template

---
+++ src/Controller/MedicalVisitController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MedicalVisit;
+use App\Entity\Patient;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MedicalVisitController extends AbstractController
+{
+    /**
+     * @Route("/medical/visits/{patient}", name="medical_visit")
+     */
+    public function index(Patient $patient): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(MedicalVisit::class);
+        return $this->render('medical_visit/index.html.twig', [
+            "medicalVisits" => $repository->findAll()
+        ]);
+    }
+}
