bfefa064d5732cfdb1566297b0064444e0167ebe
Daniel <dandys9987@gmail.com>
2022-04-05 11:57:47 +0200

    :sparkles: added possibility to create medical visit

--- src/Controller/MedicalVisitController.php
+++ src/Controller/MedicalVisitController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Doctor;
 use App\Entity\MedicalVisit;
 use App\Entity\Patient;
+use App\Form\MedicalVisitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +21,31 @@ class MedicalVisitController extends AbstractController
         $repository = $this->getDoctrine()->getRepository(MedicalVisit::class);
         return $this->render('medical_visit/index.html.twig', [
             "medicalVisits" => $repository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/medical/visits/add/{patient}/{doctor}", name="add_medical_visit")
+     */
+    public function add(Request $request,Patient $patient, Doctor $doctor): Response
+    {
+        $medicalVisit = new MedicalVisit();
+        $form = $this->createForm(MedicalVisitType::class, $medicalVisit);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $medicalVisit
+                ->setDoctor($doctor)
+                ->setPatient($patient)
+                ->setDate(date_create(date("d-m-Y")));
+            $entityManager=$this->getDoctrine()->getManager();
+            $entityManager->persist($medicalVisit);
+            $entityManager->flush();
+            $this->addFlash('success', 'Pomyślnie dodano wizytę');
+            return $this->redirectToRoute('patients');
+        }
+        return $this->render('medical_visit/add.html.twig', [
+            "form" => $form->createView(),
+            "patient" => $patient
         ]);
     }
 }
